8ea36fc202cdeca99a701ecb510b19f7cd889511
rookxd <muzafferpro323232@gmail.com>
2024-09-29 02:59:09 +0300

    Add files via upload

---
+++ rook-antibadcop/client.lua
@@ -0,0 +1,36 @@
+QBCore = exports['qb-core']:GetCoreObject()
+
+
+PlayerData = QBCore.Functions.GetPlayerData()
+
+CreateThread(function()
+    while true do
+        if IsPedArmed(PlayerPedId(), 4) then
+            local _, silah = GetCurrentPedWeapon(PlayerPedId())
+            local silahaq = false
+            local meslekaq = false
+            local PlayerData = QBCore.Functions.GetPlayerData()
+            for i = 1, #Config.Weapons do
+                if silah == Config.Weapons[i] then
+                    silahaq = true
+                    break
+                end
+            end
+            for i = 1, #Config.Jobs do
+                if PlayerData.job.name == Config.Jobs[i] then
+                    meslekaq = true
+                    break
+                end
+            end
+            if silahaq and not meslekaq then
+                QBCore.Functions.Notify(Config.Locale['notify'], "error")
+                if Config.Inventory == 'ox' then
+                    TriggerEvent('ox_inventory:disarm', true)
+                elseif Config.Inventory == 'qb' then
+                    SetCurrentPedWeapon(PlayerPedId(), `WEAPON_UNARMED`, true)
+                end
+            end
+        end
+        Wait(1000)
+    end
+end)
---
+++ rook-antibadcop/config.lua
@@ -0,0 +1,22 @@
+Config = {}
+
+Config.Inventory = 'ox' -- 'qb'  'ox'
+
+Config.Weapons = {
+    `WEAPON_APPISTOL`,
+    `WEAPON_PISTOL`,
+}
+
+Config.Jobs = {
+    "ambulance",
+    "police",
+    "bcso",
+    "sast",
+    "sasp",
+    "lssd",
+    "sapr"
+}
+
+Config.Locale = {
+    notify = 'Polis silahını kullanamazsın'
+}
